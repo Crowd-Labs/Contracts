79e0d42dc48a8d55e9dc1d55e276fa1c498a9949
leven <leven890619@gmail.com>
2024-01-19 22:17:28 +0800

    remove unuse file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.19',
+        version: '0.8.18',
         settings: {
           optimizer: {
             enabled: true,
